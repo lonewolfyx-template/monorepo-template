1a9037a8e905a02230fd6782b3f65a6108d4de8a
lonewolfyx <877085893@qq.com>
2025-12-14 00:26:31 +0800

    chore(eslint): config YAML file parsing and code checking rules

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,9 +1,28 @@
 import antfu from '@antfu/eslint-config'
+import yamlParser from 'yaml-eslint-parser'
 
 export default antfu({
     type: 'lib',
     stylistic: {
         indent: 4,
         quotes: 'single',
+    },
+    rules: {
+        'no-console': 'off',
+        'node/prefer-global/process': 'off',
+        'ts/explicit-function-return-type': 'off',
+        'vue/block-order': 'off',
+        'vue/valid-template-root': 'off',
+        'unused-imports/no-unused-vars': 'off',
+        'antfu/top-level-function': 'off',
+        'regexp/no-unused-capturing-group': 'off',
+    },
+}, {
+    files: ['**/*.yml', '**/*.yaml'],
+    languageOptions: {
+        parser: yamlParser,
+    },
+    rules: {
+        'yaml/indent': ['error', 2],
     },
 })
